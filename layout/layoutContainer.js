fa4eb23805cf72b9c97e0e06c2b71e8061c6fa1d
eric-wang <starwz@gmail.com>
2011-11-08 16:22:05 +0800

    replace 4 space to tab in layoutContainer.js

--- layout/layoutContainer.js
+++ layout/layoutContainer.js
@@ -1,13 +1,14 @@
-define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "./_layoutMixin"], function(declare, Widget, Container, Contained, layoutMixin){
-    return declare("dojox.app.layout.layoutContainer", [Widget, Container, Contained, layoutMixin], {
-        startup: function(){
-            if (this._started) {
-                return;
-            }
-            this._started = true;
+define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "./_layoutMixin"], 
+function(declare, Widget, Container, Contained, layoutMixin){
+	return declare("dojox.app.layout.layoutContainer", [Widget, Container, Contained, layoutMixin], {
+		startup: function(){
+			if (this._started) {
+				return;
+			}
+			this._started = true;
 
-            // call _layoutMixin startup to layout children
-            this.inherited(arguments);
-        }
-    });
+			// call _layoutMixin startup to layout children
+			this.inherited(arguments);
+		}
+	});
 });
