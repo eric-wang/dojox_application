09f6ef8ec11faf19989a5d62d3eeb99e576719e8
eric-wang <starwz@gmail.com>
2011-11-08 15:57:06 +0800

    Add _layoutMixin to dojox.app.
    Also add a simple layoutContainer for testcase.

---
+++ layout/layoutContainer.js
@@ -0,0 +1,13 @@
+define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "./_layoutMixin"], function(declare, Widget, Container, Contained, layoutMixin){
+    return declare("dojox.app.layout.layoutContainer", [Widget, Container, Contained, layoutMixin], {
+        startup: function(){
+            if (this._started) {
+                return;
+            }
+            this._started = true;
+
+            // call _layoutMixin startup to layout children
+            this.inherited(arguments);
+        }
+    });
+});
