fb89652e04425671c92d7e5e4f0a8711998e5163
eric-wang <starwz@gmail.com>
2011-11-08 15:59:46 +0800

    add content page for dojox.app do layout when we need non-scrollable container.

---
+++ pane.js
@@ -0,0 +1,9 @@
+define(["dojo/_base/declare",
+	"dijit/_Contained",
+	"dijit/_Container",
+	"dijit/_WidgetBase"],
+	function(declare, WidgetBase, Container, Contained){
+	return declare("dojox.app.pane", [WidgetBase, Container, Contained], {
+
+	});
+});
