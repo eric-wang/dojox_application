f752db6c2d98c844da26a75377b07bbf475972d4
eric-wang <starwz@gmail.com>
2011-11-09 13:28:10 +0800

    Mixin _layoutMixin to dojox.app.pane
    To do inner layout in scene or view.
    An alternative is provide layoutContainer module to do layout
    and each view/scene can contains more than one layoutContainer.

--- pane.js
+++ pane.js
@@ -1,9 +1,18 @@
 define(["dojo/_base/declare",
 	"dijit/_Contained",
 	"dijit/_Container",
-	"dijit/_WidgetBase"],
-	function(declare, WidgetBase, Container, Contained){
-	return declare("dojox.app.pane", [WidgetBase, Container, Contained], {
+	"dijit/_WidgetBase",
+	"./layout/_layoutMixin"],
+	function(declare, WidgetBase, Container, Contained, layoutMixin){
+	return declare("dojox.app.pane", [WidgetBase, Container, Contained, layoutMixin], {
+		startup: function(){
+			if (this._started) {
+				return;
+			}
+			this._started = true;
 
+			// call _layoutMixin startup to layout children
+			this.inherited(arguments);
+		}
 	});
 });
