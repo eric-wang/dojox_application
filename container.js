3e89d498b47443bd031637af21067923b03eb353
eric-wang <starwz@gmail.com>
2011-11-29 16:43:02 +0800

    merge dojox.app.container and dojo.app.pane
    container can be used as a layout container by default.
    If need scrollable feature, set container property scrollable="true"

--- container.js
+++ container.js
@@ -1,10 +1,34 @@
-define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "./widget/_ScrollableMixin"], function(declare, WidgetBase, Container, Contained, ScrollableMixin){
-    return declare("dojox.app.container", [WidgetBase, Container, Contained, ScrollableMixin], {
+define(["dojo/_base/declare", "dojo/_base/lang", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "./widget/_ScrollableMixin","./layout/_layoutMixin"], 
+function(declare, lang, WidgetBase, Container, Contained, ScrollableMixin, layoutMixin){
+    return declare("dojox.app.container", [WidgetBase, Container, Contained, layoutMixin], {
         buildRendering: function(){ 
 			this.inherited(arguments);
-			this.domNode.style.position = "absolute";
-			this.domNode.style.width = "100%";
-			this.domNode.style.height = "100%";
-        }
+			// Mixin _scrollableMixin if the container scrollable="true"
+			if(this.scrollable){
+				var scrollableMixin = new ScrollableMixin();
+				lang.mixin(this, scrollableMixin);
+
+				// Fix scrollDir for the mixin will set scrollDir to v
+				if(this.scrolldir){
+					this.scrollDir = this.scrolldir;
+				}
+
+				// build scrollable container
+				this.inherited(arguments);
+				this.domNode.style.position = "absolute";
+				this.domNode.style.width = "100%";
+				this.domNode.style.height = "100%";
+			}
+        },
+
+		startup: function(){
+			if (this._started) {
+				return;
+			}
+			this._started = true;
+
+			// call _layoutMixin startup to layout children
+			this.inherited(arguments);
+		}
     });
 });
