47e269fd384ff94095655b1d004f460caecb8e76
eric-wang <starwz@gmail.com>
2011-12-06 15:00:16 +0800

    Add json data model support in dojox.app model module

--- model.js
+++ model.js
@@ -1,4 +1,5 @@
-define(["dojo/_base/kernel","dojo/_base/Deferred","dojox/mvc/StatefulModel"], function(dojo,deferred){
+define(["dojo/_base/kernel", "dojo/_base/Deferred", "dojo/store/DataStore", "dojox/mvc/StatefulModel"],
+function(dojo, deferred, dataStore){
 	return function(config, parent){
                 //load models here. create dojox.newStatefulModel 
                 //using the configuration data for models
@@ -10,16 +11,29 @@ define(["dojo/_base/kernel","dojo/_base/Deferred","dojox/mvc/StatefulModel"], fu
                     for(var item in config){
                         if(item.charAt(0)!=="_"){
                             var params = config[item].params ? config[item].params:{};
-                            var options = {
-                                "store": params.store.store,
-                                "query": params.store.query ? params.store.query : {}
-                            };
-                            
-                            //TODO improve performance of loading at here
-                            // do not wait for the models to be created.
-                            loadedModels[item] = deferred.when(dojox.mvc.newStatefulModel(options), function(model){return model});
-                        }
-                    }
+					var options;
+					if (params.store.params.data) {
+						options = {
+							"store": params.store.store,
+							"query": params.store.query ? params.store.query : {}
+						};
+					}
+					else if (params.store.params.url) {
+						options = {
+							"store": new dataStore({
+								store: params.store.store
+							}),
+							"query": params.store.query ? params.store.query : {}
+						};
+					}
+
+					//TODO improve performance of loading at here
+					// do not wait for the models to be created.
+					loadedModels[item] = deferred.when(dojox.mvc.newStatefulModel(options), function(model){
+						return model;
+					});
+				}
+			}
 	        }
                 return loadedModels;
 	}
