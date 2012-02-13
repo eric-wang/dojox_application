302c09d5168176e7f76de0806dc3dc6211cd4d0e
eric <eric@Eric-IBM.(none)>
2012-02-13 13:12:46 +0800

    update application main and view

--- main.js
+++ main.js
@@ -1,114 +1,123 @@
 define(["dojo/_base/lang",
-	"dojo/_base/declare",
-	"dojo/_base/Deferred",
-	"dojo/_base/connect",
-	"dojo/ready",
-	"dojo/_base/window",
-	"dojo/dom-construct",
-	"./scene"],
-	function(dlang, declare, deferred, connect, ready, baseWindow, dom, sceneCtor){
+"dojo/_base/declare",
+"dojo/_base/Deferred",
+"dojo/_base/connect",
+"dojo/ready",
+"dojo/_base/window",
+"dojo/dom-construct",
+"./controller",
+"./utils/model",
+"dojo/store/Memory"], 
+function(lang, declare, deferred, connect, ready, baseWindow, dom, Controller, Model){
+	dojo.experimental("dojox.app");
 
-        dojo.experimental("dojox.app");
-	var Application = declare([sceneCtor], {
-		constructor: function(params){
-			this.scenes={};
-			if(params.stores){
-			    //create stores in the configuration.
-			    for (var item in params.stores){
-			        if(item.charAt(0)!=="_"){//skip the private properties
-			            var type = params.stores[item].type? params.stores[item].type : "dojo.store.Memory";
-			            var config = {};
-			            if(params.stores[item].params){
-			                dlang.mixin(config, params.stores[item].params);
-			            }
-			            var storeCtor = dojo.getObject(type);
-			            if(config.data && dlang.isString(config.data)){
-			                //get the object specified by string value of data property
-			                //cannot assign object literal or reference to data property
-			                //because json.ref will generate __parent to point to its parent
-			                //and will cause infinitive loop when creating StatefulModel.
-			                config.data = dlang.getObject(config.data);
-			            }
-			            params.stores[item].store = new storeCtor(config);
-			        }
-			    }
+	var Application = declare([], {
+		constructor: function(params, node){
+			lang.mixin(this, params);
+			this.params = params;
+			this.stores = {};
+			this.srcNodeRef = node;
+			this.domNode = node;
+			this.id = params.id;
+			this.defaultView = params.defaultView;
+			this.widgetId = params.id;
+			// this.controller = new Controller(this);
+			// mixin controller to application
+			lang.mixin(this, new Controller(this));
+		},
+
+		createDataStore: function(params){
+			if (params.stores) {
+				//create stores in the configuration.
+				for (var item in params.stores) {
+					if (item.charAt(0) !== "_") {//skip the private properties
+						var type = params.stores[item].type ? params.stores[item].type : "dojo.store.Memory";
+						var config = {};
+						if (params.stores[item].params) {
+							lang.mixin(config, params.stores[item].params);
+						}
+						var storeCtor = lang.getObject(type);
+						if (config.data && lang.isString(config.data)) {
+							//get the object specified by string value of data property
+							//cannot assign object literal or reference to data property
+							//because json.ref will generate __parent to point to its parent
+							//and will cause infinitive loop when creating StatefulModel.
+							config.data = lang.getObject(config.data);
+						}
+						params.stores[item].store = new storeCtor(config);
+						this.stores[item] = params.stores[item].store;
+					}
+				}
 			}
-
 		},
 
 		// load default view and startup the default view
-        start: function(applicaton){
-            var child = this.loadChild();
-
-            deferred.when(child, dlang.hitch(this, function(){
-                this.startup();
-
-                //set application status to STARTED
-                this.setStatus(this.lifecycle.STARTED);
-            }));
-        },
-		templateString: "<div></div>",
-		selectedChild: null,
-		baseClass: "application mblView",
-		defaultViewType: sceneCtor,
-		buildRendering: function(){
-			if (this.srcNodeRef===baseWindow.body()){
-				this.srcNodeRef = dom.create("DIV",{},baseWindow.body());
+		start: function(applicaton){
+			var path = window.location.pathname;
+			if (path.charAt(path.length) != "/") {
+				path = path.split("/");
+				path.pop();
+				path = path.join("/");
 			}
-			this.inherited(arguments);
+			dojo.registerModulePath("app", path);
+
+			this.postCreate();
+			this.createDataStore(this.params);
+			this.loadedModels = Model(this.params.models, this);
+			var currentView = this.loadView(this.defaultView);
+
+			deferred.when(currentView, lang.hitch(this, function(result){
+				this.selectedView = result;
+				this.setStatus(this.lifecycle.STARTED);
+			}));
+			console.log("application started.");
 		}
 	});
-	
-	function generateApp(config,node,appSchema,validate){
 
-		//console.log("config.modules: ", config.modules);
+	function generateApp(config, node, appSchema, validate){
 		var modules = config.modules.concat(config.dependencies);
 
-		if (config.template){
-			//console.log("config.template: ", config.template);
+		if (config.template) {
 			modules.push("dojo/text!" + "app/" + config.template);
 		}
-		//console.log("modules: ", modules);	
 
 		require(modules, function(){
-			var modules=[Application];
-			for(var i=0;i<config.modules.length;i++){
+			var modules = [Application];
+			for (var i = 0; i < config.modules.length; i++) {
 				modules.push(arguments[i]);
 			}
 
-			if (config.template){
-				var ext = {
-					templateString: arguments[arguments.length-1] 
-				}	
+			var ext = {};
+			if (config.template) {
+				ext = {
+					templateString: arguments[arguments.length - 1]
+				};
 			}
-			App = declare(modules,ext);
+			App = declare(modules, ext);
 
 			ready(function(){
-				app = App(config,node || baseWindow.body());
-                app.setStatus(app.lifecycle.STARTING);
-                app.start();
+				app = App(config, node || baseWindow.body());
+				app.setStatus(app.lifecycle.STARTING);
+				app.start();
 			});
 		});
-	}
+	};
 
-
-	return function(config,node){
-		if (!config){
+	return function(config, node){
+		if (!config) {
 			throw Error("App Config Missing");
 		}
 
-		
-		if (config.validate){
-			require(["dojox/json/schema","dojox/json/ref","dojo/text!dojox/application/schema/application.json"],function(schema,appSchema){
-				schema = dojox.json.ref.resolveJson(schema);	
-				if (schema.validate(config,appSchema)){
-					generateApp(config,node);
-				}	
+		if (config.validate) {
+			require(["dojox/json/schema", "dojox/json/ref", "dojo/text!dojox/application/schema/application.json"], function(schema, appSchema){
+				schema = dojox.json.ref.resolveJson(schema);
+				if (schema.validate(config, appSchema)) {
+					generateApp(config, node);
+				}
 			});
-		
-
-		}else{
-			generateApp(config,node);
 		}
-	}
+		else {
+			generateApp(config, node);
+		}
+	};
 });
--- view.js
+++ view.js
@@ -1,15 +1,120 @@
-define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained","dijit/_TemplatedMixin","dijit/_WidgetsInTemplateMixin"],function(declare,Widget,Container,Contained,TemplatedMixin,WidgetsInTemplateMixin){
-	return declare("dojox.app.view", [Widget,TemplatedMixin,Container,Contained, WidgetsInTemplateMixin], {
-		selected: false,
-		keepScrollPosition: true,
-		baseClass: "applicationView mblView",
-		config:null,
-		widgetsInTemplate: true,
-		templateString: '<div></div>',
-		toString: function(){return this.id},
-		activate:function(){},
-		deactivate: function(){},
-		//Temporary work around for getting a null when calling getParent
-		getParent: function(){return null;}
+define(["dojo/_base/declare",
+"dojo/_base/lang",
+"dojo/_base/Deferred",
+"dojo/parser",
+"dojo/_base/connect",
+"dojo/dom-construct",
+"dijit/_TemplatedMixin",
+"dijit/_WidgetsInTemplateMixin",
+"./viewcontroller",
+"./utils/model",
+"./utils/bind"],
+function(declare, lang, deferred, parser, connect, domConstruct, TemplatedMixin, WidgetsInTemplateMixin, ViewController, Model, Bind){
+	return declare("dojox.application.view", [ViewController], {
+		constructor: function(params){
+			this.id = "";
+			this.name = "";
+			this.templateString = "";
+			this.parent = null;
+			this._start = false;
+
+			lang.mixin(this, params);
+			if (this.parent) {
+				lang.mixin(this, this.parent.params.views[this.name]);
+			}
+		},
+
+		// start view
+		start: function(){
+			if (this._start) {
+				return;
+			}
+
+			// create DOM node from templateString
+			if (this.templateString) {
+				// TODO: create widget 
+				this.widget = this.render(this.templateString);
+				this.domNode = this.widget.domNode;
+				this.parent.domNode.appendChild(this.domNode);
+			}
+			// load from templateString
+			else {
+				console.log("load templateString from template file. ", this.params);
+				if (!this.dependencies) {
+					this.dependencies = [];
+				}
+				// load viewcontroller
+				var viewController = this.controller;
+				if (viewController) {
+					var index = viewController.indexOf('.js');
+					if (index != -1) {
+						viewController = viewController.substring(0, index);
+					}
+					this.dependencies = ["app/" + viewController].concat(this.dependencies);
+				}
+
+				var deps = this.template ? this.dependencies.concat(["dojo/text!app/" + this.template]) : this.dependencies.concat([]);
+				var def = new deferred();
+				if (deps.length > 0) {
+					require(deps, function(){
+						def.resolve.call(def, arguments);
+					});
+				}
+				else {
+					def.resolve(true);
+				}
+
+				var loadViewDeferred = new deferred();
+				deferred.when(def, lang.hitch(this, function(){
+					this.templateString = this.template ? arguments[0][arguments[0].length - 1] : "<div></div>";
+					this.widget = this.render(this.templateString);
+					this.widget.id = this.id;
+					//Todo: create view data model
+
+					//Todo: bind data to widget
+					this.bindModel(this.widget);
+					this.domNode = this.widget.domNode;
+					this.parent.domNode.appendChild(this.domNode);
+
+					//start widget
+					this.widget.startup();
+
+					//mixin view controller, controller is the first object of arguements
+					lang.mixin(this, arguments[0][0]);
+					// call view controller's init() method to initialize view
+					this.init();
+					this._start = true;
+					loadViewDeferred.resolve(this);
+				}));
+				return loadViewDeferred;
+			}
+		},
+
+		render: function(templateString){
+			var widgetTemplate = new TemplatedMixin();
+			var widgetInTemplate = new WidgetsInTemplateMixin();
+			lang.mixin(widgetTemplate, widgetInTemplate);
+			widgetTemplate.templateString = templateString;
+			widgetTemplate.buildRendering();
+			return widgetTemplate;
+		},
+
+		// select current view
+		select: function(){
+			console.log("select view: " + this.id);
+		},
+
+		unselect: function(){
+			console.log("unselect view: " + this.id);
+		},
+
+		bindModel: function(widget){
+			//load child's model if it is not loaded before
+			if (!this.loadedModels) {
+				this.loadedModels = Model(this.models, this.parent);
+				//TODO need to find out a better way to get all bindable controls in a view
+				Bind([widget], this.loadedModels);
+			}
+		}
 	});
 });
