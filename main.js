0b7243f2fc4f10c846fa3d7da702c47c8dfa8229
eric-wang <starwz@gmail.com>
2012-02-21 15:48:19 +0800

    refactor dojox.app.main bind data to application.evented load application template/assistant

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 define(["dojo/_base/lang",
 "dojo/_base/declare",
 "dojo/_base/Deferred",
-"dojo/_base/connect",
+"dojo/on",
+"dojo/Evented",
 "dojo/ready",
 "dojo/_base/window",
 "dojo/dom-construct",
-"./controller",
 "./utils/model",
-"dojo/store/Memory"], 
-function(lang, declare, deferred, connect, ready, baseWindow, dom, Controller, Model){
+"./view",
+"dojo/_base/loader",
+"dojo/store/Memory"],
+function(lang, declare, deferred, on, Evented, ready, baseWindow, dom, Model, View){
 	dojo.experimental("dojox.app");
 
 	var Application = declare([], {
@@ -16,14 +18,21 @@ function(lang, declare, deferred, connect, ready, baseWindow, dom, Controller, M
 			lang.mixin(this, params);
 			this.params = params;
 			this.stores = {};
-			this.srcNodeRef = node;
-			this.domNode = node;
 			this.id = params.id;
 			this.defaultView = params.defaultView;
 			this.widgetId = params.id;
-			// this.controller = new Controller(this);
-			// mixin controller to application
-			lang.mixin(this, new Controller(this));
+			this.controllers = [];
+			this.children = {};
+			this.loadedModels = {};
+			this.evented = new Evented();
+
+			// Create a new domNode and append to body
+			// Need to bind startTransition event on application domNode,
+			// Because dojox.mobile.ViewController bind startTransition event on document.body
+			this.domNode = dom.create("div", {
+				id: this.id
+			});
+			node.appendChild(this.domNode);
 		},
 
 		createDataStore: function(params){
@@ -51,35 +60,88 @@ function(lang, declare, deferred, connect, ready, baseWindow, dom, Controller, M
 			}
 		},
 
+		createControllers: function(controllers){
+			if (controllers) {
+				var requireItems = [];
+				for (var i = 0; i < controllers.length; i++) {
+					requireItems.push(controllers[i]);
+				}
+
+				var def = new deferred();
+				require(requireItems, function(){
+					def.resolve.call(def, arguments);
+				})
+
+				var controllerDef = new deferred();
+				deferred.when(def, lang.hitch(this, function(){
+					for (var i = 0; i < arguments[0].length; i++) {
+						// Store Application object on each application level controller.
+						this.controllers.push(new arguments[0][i](this));
+					}
+					controllerDef.resolve(this);
+				}));
+				return controllerDef;
+			}
+		},
+
 		// load default view and startup the default view
 		start: function(applicaton){
-			var path = window.location.pathname;
-			if (path.charAt(path.length) != "/") {
-				path = path.split("/");
-				path.pop();
-				path = path.join("/");
-			}
-			dojo.registerModulePath("app", path);
-
-			this.postCreate();
+			//create application level data store
 			this.createDataStore(this.params);
-			this.loadedModels = Model(this.params.models, this);
-			var currentView = this.loadView(this.defaultView);
 
-			deferred.when(currentView, lang.hitch(this, function(result){
-				this.selectedView = result;
-				this.setStatus(this.lifecycle.STARTED);
+			// create application level data model
+			this.loadedModels = Model(this.params.models, this);
+
+			// create application template view
+			if(this.template){
+				this.view = new View({
+					id: this.id,
+					name: this.name,
+					parent: this,
+					templateString: this.templateString,
+					assistant: this.assistant
+				});
+				this.view.start();
+			}
+
+			// create application level controller
+			var controllers = this.createControllers(this.params.controllers);
+
+			deferred.when(controllers, lang.hitch(this, function(result){
+				// emit loadview event and let controller to load view.
+				on.emit(this.evented, "loadView", {
+					viewId: this.defaultView,
+					parent: this
+				});
+				this.selectedView = this.children[this.id + '_' + this.defaultView];
+				deferred.when(this.selectedView.start(), lang.hitch(this, function(){
+					this.setStatus(this.lifecycle.STARTED);
+					this.selectedView.select();
+					console.log("application started.");
+				}));
 			}));
-			console.log("application started.");
 		}
 	});
 
 	function generateApp(config, node, appSchema, validate){
+		// Register application module path
+		var path = window.location.pathname;
+		if (path.charAt(path.length) != "/") {
+			path = path.split("/");
+			path.pop();
+			path = path.join("/");
+		}
+		dojo.registerModulePath("app", path);
+
 		var modules = config.modules.concat(config.dependencies);
 
 		if (config.template) {
 			modules.push("dojo/text!" + "app/" + config.template);
 		}
+		//TODO: load view assistant if exists in configure
+//		if (config.assistant){
+//			modules.push("dojo/text!" + "app/" + config.assistant);
+//		}
 
 		require(modules, function(){
 			var modules = [Application];
